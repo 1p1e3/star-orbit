3278a4e4c861581ad4a43425de8ef7df9e74a9d6
chris <2762851151@qq.com>
2025-03-09 18:54:06 +0800

    :art:update path

--- config/path_conf.py
+++ config/path_conf.py
@@ -9,8 +9,15 @@ core_path = root_path.joinpath('core')
 # prompts 目录
 prompts_path = root_path.joinpath('prompts')
 
+# 用于生成用例的提示词路径
 case_generate_prompt_path = prompts_path.joinpath('case_generate.md')
 
 # case
-cases_json_path = root_path.joinpath('output.json')
+cases_json_path = root_path.joinpath('cases', 'cases.json')
+
+# api 接口文档目录
+api_docs_path = root_path.joinpath('api_docs', "api_doc.json")
+
+# 测试报告和结果输出目录
+output_path = root_path.joinpath('output')
 
